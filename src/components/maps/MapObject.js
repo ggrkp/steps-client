3be1491fbf5ac9ffc6f37b208e8560c0a06d0263
Georgakopoulos Giorgos <ggeorg.prime@gmail.com>
2022-07-08 15:35:24 +0300

    added leaflet map with heatmap layer

--- src/components/maps/MapObject.js
+++ src/components/maps/MapObject.js
@@ -1,56 +1,69 @@
-import React, { useState } from 'react'
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import React, { useState, useEffect } from 'react'
+// import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 import ProfileCard from '../layout/ProfileCard'
+import HeatmapOverlay from './heatmap-leaflet.js'
+import L from 'leaflet'
 
 const MapObject = () => {
-    
+    useEffect(() => {
 
-    const [position, setPosition] = useState([38.0110, 23.6887])
-    const [zoom, setZoom] = useState(13)
+        // const map = Leaflet.map('map').setView([51.505, -0.09], 13);
+        // Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        //     maxZoom: 19,
+        //     attribution: '© OpenStreetMap'
+        // }).addTo(map);
+
+        // don't forget to include leaflet-heatmap.js
+        var testData = {
+            max: 8,
+            data: [{ lat: 24.6408, lng: 46.7728, count: 3 }, { lat: 50.75, lng: -1.55, count: 1 }]
+        };
+
+        var baseLayer = L.tileLayer(
+            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '...',
+            maxZoom: 18
+        }
+        );
+
+        var cfg = {
+            // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+            // if scaleRadius is false it will be the constant radius used in pixels
+            "radius": 2,
+            "maxOpacity": .8,
+            // scales the radius based on map zoom
+            "scaleRadius": true,
+            // if set to false the heatmap uses the global maximum for colorization
+            // if activated: uses the data maximum within the current map boundaries
+            //   (there will always be a red spot with useLocalExtremas true)
+            "useLocalExtrema": true,
+            // which field name in your data represents the latitude - default "lat"
+            latField: 'lat',
+            // which field name in your data represents the longitude - default "lng"
+            lngField: 'lng',
+            // which field name in your data represents the data value - default "value"
+            valueField: 'count'
+        };
+
+
+        var heatmapLayer = new HeatmapOverlay(cfg);
+
+        var map = new L.Map('map', {
+            center: new L.LatLng(25.6586, -80.3568),
+            zoom: 4,
+            layers: [baseLayer, heatmapLayer]
+        });
+
+        heatmapLayer.setData(testData);
+    }, [])
+
     return (
-        <ProfileCard title={"Heatmap"}>
-            <MapContainer className="map-container" center={position} zoom={zoom}>
-                <TileLayer
-                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-            </MapContainer>
+        <ProfileCard title="Heatmap">
+            <div className="map-container" id="map"></div>
         </ProfileCard>
     )
 }
 
 
-
-
-
-
-
-// class MapObject extends React.Component {
-//   constructor () {
-//     super()
-//     this.state = {
-//       lat: 51.505,
-//       lng: -0.09,
-//       zoom: 13
-//     }
-//   }
-
-//   render () {
-//     const position = [this.state.lat, this.state.lng]
-//     return (
-//         <MapContainer className="map-container" center={position} zoom={this.state.zoom}>
-//           <TileLayer
-//             url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-//             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-//           />
-//           <Marker position={position}>
-//             <Popup>
-//               <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-//             </Popup>
-//           </Marker>
-//         </MapContainer>
-//       )
-//   }
-// }
-
 export default MapObject
+
